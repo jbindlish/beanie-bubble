7196add1208467cc4a582d1682cbe7878f91652f
jbindlish <93174670+jbindlish@users.noreply.github.com>
2025-11-21 11:06:34 -0800

    Refactor main.jsx for React 18 and style imports
    Updated import statements and simplified rendering method.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './styles.css' // import styles so Vite includes them in the build and rewrites the path with base
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<App />)
